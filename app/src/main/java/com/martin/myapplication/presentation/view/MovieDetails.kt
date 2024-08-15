8357a2427c338a31d688d49492a558fc4e8315e3
Aneta Solunska <aneta.solunska@scalefocus.com>
2024-08-15 13:58:48 +0200

    added tab navigation in details page

--- app/src/main/java/com/martin/myapplication/presentation/view/MovieDetails.kt
+++ app/src/main/java/com/martin/myapplication/presentation/view/MovieDetails.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Scaffold
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,6 +25,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,6 +36,9 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -157,6 +165,10 @@ fun Details() {
 
 @Composable
 fun MovieDetailsContent(innerPadding: PaddingValues) {
+    var activeTab by remember {
+        mutableStateOf("About")
+    }
+
     Column(modifier = Modifier.fillMaxSize()) {
         Box(modifier = Modifier.fillMaxWidth()) {
             Image(
@@ -284,9 +296,78 @@ fun MovieDetailsContent(innerPadding: PaddingValues) {
                     details = true
                 )
             }
-
         }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .absoluteOffset(y = (-20).dp)
+                .padding(start = 30.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                ClickableText(
+                    onClick = {
+                        activeTab = "About"
+                    },
+                    text = AnnotatedString("About Movie"),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = poppins,
+                        fontWeight = if (activeTab == "About") FontWeight.Bold else FontWeight.Medium,
+                        color = Color.White
+                    ),
+                )
+                ClickableText(
+                    onClick = {
+                        activeTab = "Reviews"
+                    },
+                    text = AnnotatedString("Reviews"),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = poppins,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.White
+                    ),
+                )
+                ClickableText(
+                    onClick = {
+                        activeTab = "Cast"
+                    },
+                    text = AnnotatedString("Cast"),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = poppins,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.White
+                    ),
+                )
+            }
+        }
+
+        if (activeTab == "About") {
+            AboutContent()
+        } else if (activeTab == "Reviews") {
+            ReviewsContent()
+        } else
+            CastContent()
+
     }
+}
+
+@Composable
+fun AboutContent() {
+    Text(text = "About")
+}
+
+@Composable
+fun ReviewsContent() {
+    Text(text = "Reviews")
+}
+
+@Composable
+fun CastContent() {
+    Text(text = "Cast")
 }
 
 @Preview
